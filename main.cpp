a1f258e565080f863bff1198d792950bfe849ad6
whaim <xminsky@gmail.com>
2026-04-03 21:31:05 +0800

    调整文件结构

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@
 #include <format>
 #include <print>
 
-int cube();
+#include "string"
+
+std::string AppName = "01_InitInstanceRAII";
+std::string EngineName = "Vulkan.hpp";
+
 auto main()->int{
     std::println("Using cpp version {}", __cplusplus);
 
-    cube();
+    vk::raii::Context context;
+    // initialize the vk::ApplicationInfo structure
+    vk::ApplicationInfo applicationInfo( AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1 );
+
+    // initialize the vk::InstanceCreateInfo
+    vk::InstanceCreateInfo instanceCreateInfo( {}, &applicationInfo );
+
+    // create an Instance
+    vk::raii::Instance instance( context, instanceCreateInfo );
     return EXIT_SUCCESS;
 }
