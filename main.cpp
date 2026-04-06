a608f544f8d70c772199b1d8f8f1b503c2da1833
HmingWang <xminsky@gmail.com>
2026-04-06 17:37:41 +0800

    up

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ auto main() -> int {
     vk::raii::Context context;
     vk::raii::Instance instance =vk::raii::su::makeInstance(context, "HelloVulkan", "Vulkan.hpp", {}, vk::su::getInstanceExtensions());
 
+    #if !defined(NDEBUG)
+    // 2. setup debug messenger 
+    vk::raii::DebugUtilsMessengerEXT debugUtilsMessenger(instance, vk::su::makeDebugUtilsMessengerCreateInfoEXT());
+    #endif
+
+    vk::raii::su::SurfaceData surfaceData(instance, "HelloVulkan", vk::Extent2D(500, 500));
+
   } catch (vk::SystemError &err) {
     std::cerr << "vk::SystemError: " << err.what() << std::endl;
     exit(-1);
